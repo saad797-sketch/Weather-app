b4e258103db0160e94c3db3cd2dba219af292c09
saad797-sketch <sebastianarango797@gmail.com>
2026-04-15 18:41:54 -0300

    actualizacion

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const faviconUrl = new URL('../SVG/clear-svgrepo-com.svg', import.meta.url).href;
+const faviconLink = document.querySelector("link[rel='icon']") as HTMLLinkElement | null;
+if (faviconLink) {
+  faviconLink.href = faviconUrl;
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
